e436b9b2d7549c9d3a089829a04b709629d78c5e
Deva Sou <soudeva974@gmail.com>
2020-08-12 12:10:50 +0200

    adding files for models validations

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2020_08_12_093151) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
-    t.string "adress"
+    t.string "address"
     t.string "phone_number"
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
